5800c14175403b56fb0329c73bc4ab67dda73d3c
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-06-20 06:34:19 +0700

    Update index.js

--- src/handler/index.js
+++ src/handler/index.js
@@ -38,6 +38,17 @@ function getRemaining(endtime) {
      };
 }
 
+function fetchJson = (url, options) => new Promise(async (resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
 
 let handle;
 
